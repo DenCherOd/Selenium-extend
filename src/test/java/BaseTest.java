72c38c3e1efbfe5a48596179b4af2c6938ba6e66
KolyaLoveBublik <nikolay.cheran@gmail.com>
2022-08-21 14:54:34 +0300

    BaseTest commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver.quit();
+    }
+}
